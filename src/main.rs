d4edc315cdf55794e9c6f82d292aa0746b8c2a8d
fnabinash <fnabinash@gmail.com>
2024-10-30 14:14:56 +0000

    [New Command] Adding  command and some brainstromming

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,49 @@
 // Init - init the tool and find all the dot files and copy to the .adof folder
+//      - process
+//          - when running the init command it will open a window like fzf with left side containing
+//          the files name and path and right side shows the preview with synatx highlight.
+//          - it does not pull every single file, rather it only pulls the files that it finds as
+//          dot files or config files then you can use your `tab` to select files and use `arrows`
+//          to move between files after selecting all files hit `enter` to complete the
+//          initialization process
+//
+// Add - after initialization, users can easily search and add any files they want to track
+//      - process
+//          - when running `add` command it opens a window like fzf in left side file with path and
+//          in right sight the file contents with syntax highlights then you can use `tab` to
+//          select files and `enter` to complete the process
 //
 // Link - link to a github Repo
+//      - only link the repo do not push anything untill the push command is not triggeered
 //
 // push - push the changes to github Repo with commit message that include
-//          - date
-//          - time
-//          - files that are changed
+//      - process
+//          - when the user runs the push command first print a summary in a table format with file
+//          name, then changes means how many lines added and deleted.
+//          - then ask for comformation to push or not
+//          - if user says yes then push to github with commit message that contains
+//              - date
+//              - time
+//              - files that are changed with how many lines added and deleted
 //
 // unlink - unlink the github Repo
+//      - process
+//          - ask the user for conformation to unlink or not
 //
 // deploy - copy all the dot files from github and store in the local machine at perfect places
+//      - process
+//          - when user runs the deploy command first fetch the files and shows a summary of all
+//          files like
+//              - list all the files
+//              - with line count means how many lines each file contains
+//              - then the file language like rust, lua, vimrc etc
+//              - the file location where it going to save (also give the option to user to change
+//              the file location if they want to)
+//          - after this if user says yes then proceed and save files where the location it
+//          indicated
+//          - when a user deploy from other GitHub repo do not link it only deploy, only link when
+//          user runs the deploy commmand
+//          - then do a small animation to celebrate🎉
 //
 // uninstall - remove adof from your machine, while uninstalling first check if there is untracked
 // or uncommitted changes or not if not then uninstall if yes then ask for permission to forcefully
@@ -25,7 +59,7 @@
 //
 // commit - to list the last commit
 //
-// Idea - every day create a new branch on the date of that day, and commit all the changes
+// Idea - every day create a new branch with name equals to date of that day, and commit all the changes
 // thorough out the day with time then on the next day commit the changes with a squash merge with
 // all commits props like date time and the files that are changed, also if they can they can
 // revert to previous changes made in that day or on the any day they want to
@@ -48,10 +82,13 @@ enum Commands {
     /// Initialize Adof in your system
     Init,
 
+    /// Manually add any files you want to keep track of
+    Add,
+
     /// Link to a GitHub repo to store your dotfiles
     Link {
         /// Link of the GitHub repo
-        link: String
+        link: String,
     },
 
     /// Push the local changes to GitHub
@@ -67,15 +104,15 @@ enum Commands {
         number: u8,
     },
 
-    /// Unlink with the GitHub repo
-    Unlink,
-
     /// Deploy the dot files to your system
     Deploy {
         /// Deploy the dot files from a specific commit hash
         #[arg(default_value = "Latest commit")]
         commit: String,
     },
+
+    /// Unlink with the GitHub repo
+    Unlink,
 
     /// Uninstall Adof
     Uninstall,
@@ -93,10 +130,14 @@ fn main() {
             println!("Initializing adof.");
         }
 
+        Commands::Add => {
+            println!("Manually adding new files.");
+        }
+
         Commands::Link { link } => {
             println!("Linking with GitHub Repo. Link: {:?}", link);
         }
-        
+
         Commands::Push => {
             println!("Pushing changes to GitHub");
         }
@@ -109,12 +150,15 @@ fn main() {
             println!("Printing last {} commits", number);
         }
 
-        Commands::Unlink => {
-            println!("Unlinked with GitHub Repo");
+        Commands::Deploy { commit } => {
+            println!(
+                "Deploying changes to local system. Commit hash: {:?}",
+                commit
+            );
         }
 
-        Commands::Deploy { commit } => {
-            println!("Deploying changes to local system. Commit hash: {:?}", commit);
+        Commands::Unlink => {
+            println!("Unlinked with GitHub Repo");
         }
 
         Commands::Uninstall => {
