6d3c877d1b720451225036be2ef69ca8293c671d
fnabinash <fnabinash@gmail.com>
2024-11-01 06:44:12 +0000

    [Database] Create database and functions to interact

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,43 +1,47 @@
-use std::env;
+use std::fs;
 use std::io::Write;
-use std::path::PathBuf;
+use std::path::{self, PathBuf};
 use std::process::{Command, Stdio};
 
 use glob::glob;
 
+use super::*;
+
+const FILE_PATTERN: [&str; 17] = [
+    ".bashrc",
+    ".zshrc",
+    ".bash_profile",
+    ".profile",
+    ".vimrc",
+    ".gitconfig",
+    ".config/nvim/**/*.vim",
+    ".config/git/config",
+    ".cargo/config",
+    ".config/starship.toml",
+    ".config/tmux/tmux.conf",
+    ".config/alacritty/alacritty.yml",
+    ".docker/config.json",
+    ".npmrc",
+    ".yarnrc",
+    ".config/wezterm/wezterm.lua",
+    ".config/nvim/**/*.lua",
+];
+
 pub fn init() {
-    let found_dotfiles = find_dotfiles();
-    let selected_dotfiles = show_them_in_fzf(found_dotfiles);
-    let adof_dir_path = create_adof_dir();
-    create_selected_dotfiles(&selected_dotfiles, &adof_dir_path);
+    let found_files = find_files();
+    let selected_files = select_files(found_files);
+
+    create_adof_dir();
+
+    create_copy_selected_files(&selected_files);
 }
 
-fn find_dotfiles() -> Vec<PathBuf> {
-    let home_dir = env::var("HOME").expect("Could not retrieve home directory");
-
-    let dotfiles_patterns = vec![
-        ".bashrc",
-        ".zshrc",
-        ".bash_profile",
-        ".profile",
-        ".vimrc",
-        ".gitconfig",
-        ".config/nvim/**/*.vim",
-        ".config/git/config",
-        ".cargo/config",
-        ".config/starship.toml",
-        ".config/tmux/tmux.conf",
-        ".config/alacritty/alacritty.yml",
-        ".docker/config.json",
-        ".npmrc",
-        ".yarnrc",
-        ".config/wezterm/wezterm.lua",
-        ".config/nvim/**/*.lua",
-    ];
+fn find_files() -> Vec<PathBuf> {
+    let home_dir = get_home_dir();
 
     let mut found_files = Vec::new();
 
-    for pattern in dotfiles_patterns {
+    for pattern in FILE_PATTERN {
         let pattern_path = format!("{}/{}", home_dir, pattern);
         for entry in glob(&pattern_path).expect("Failed to read glob pattern") {
             match entry {
@@ -50,8 +54,8 @@ fn find_dotfiles() -> Vec<PathBuf> {
     found_files
 }
 
-fn show_them_in_fzf(found_dotfiles: Vec<PathBuf>) -> Vec<String> {
-    let files_input = found_dotfiles
+fn select_files(found_files: Vec<PathBuf>) -> Vec<String> {
+    let files_input = found_files
         .iter()
         .map(|file| file.clone().into_os_string().into_string().unwrap())
         .collect::<Vec<String>>()
@@ -88,29 +92,26 @@ fn show_them_in_fzf(found_dotfiles: Vec<PathBuf>) -> Vec<String> {
     selected_files
 }
 
-fn create_adof_dir() -> String {
-    let home_dir = env::var("HOME").expect("Failed to retrieve home dir.");
-    let adof_dir = format!("{}/{}", home_dir, ".adof");
-    std::fs::create_dir_all(&adof_dir).expect("failed to create adof dir.");
-    adof_dir
+fn create_file(file: &str) {
+    let path = path::Path::new(file);
+    let path_dir = path.parent().unwrap();
+    fs::create_dir_all(path_dir).unwrap();
+    fs::File::create(file).unwrap();
 }
 
-fn create_selected_dotfiles(selected_dotfiles: &Vec<String>, adof_dir_path: &str) {
-    let home_dir = env::var("HOME")
-        .expect("Failed to retrieve home dir.")
-        .to_string();
+fn create_copy_selected_files(selected_files: &Vec<String>) {
+    let home_dir = get_home_dir();
+    let adof_dir = get_adof_dir();
 
-    let files_to_create = selected_dotfiles
+    let files_to_create = selected_files
         .iter()
-        .map(|file| file.replace(&home_dir, adof_dir_path))
+        .map(|file| file.replace(&home_dir, &adof_dir))
         .collect::<Vec<String>>();
 
-    files_to_create.iter().for_each(|file| {
-        std::fs::File::create(file).unwrap();
-    });
-
     (0..files_to_create.len()).for_each(|i| {
-        std::fs::copy(&selected_dotfiles[i], &files_to_create[i]).unwrap();
+        create_file(&files_to_create[i]);
+        fs::copy(&selected_files[i], &files_to_create[i]).unwrap();
+        add_files_to_database(&selected_files[i], &files_to_create[i]);
     })
 }
 
