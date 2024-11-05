3d61c5d49454f864d546f09dce41eef034420c45
fnabinash <fnabinash@gmail.com>
2024-11-05 21:10:18 +0530

    [Git] Complete branching feature

--- src/git/branch.rs
+++ src/git/branch.rs
@@ -1,5 +1,17 @@
 use crate::git::merge::merge;
 
+use super::*;
+
 pub fn create_new_branch() {
     merge();
+
+    let repo = get_repo();
+    let branch_name = get_today();
+
+    let head_commit = repo.head().unwrap().peel_to_commit().unwrap();
+    repo.branch(&branch_name, &head_commit, false).unwrap();
+
+    let branch_ref = format!("refs/heads/{}", branch_name);
+    repo.set_head(&branch_ref).unwrap();
+    repo.checkout_head(None).unwrap();
 }
