82e50efdbc8c80907e13e6680027dbe39a0962d1
fnabinash <fnabinash@gmail.com>
2024-11-07 11:14:58 +0530

    [Feat] Add feature to fetch remote commits

--- src/git/remote.rs
+++ src/git/remote.rs
@@ -1,4 +1,4 @@
-pub fn get_remote_commits() {
+pub fn get_remote_commits() -> Vec<Commit> {
     let repo = get_repo();
 
     let mut remote = repo.find_remote("origin").unwrap();
@@ -11,12 +11,18 @@ pub fn get_remote_commits() {
     revwalk.push(remote_branch.target().unwrap()).unwrap();
 
     let mut commits = Vec::new();
+
     for oid in revwalk {
         let oid = oid.unwrap();
+
         let commit = repo.find_commit(oid).unwrap();
+        let commit = Commit::new(&commit.id().to_string(), commit.message().unwrap());
         commits.push(commit);
+
         if commits.len() > 255 {
             break;
         }
     }
+
+    commits
 }
