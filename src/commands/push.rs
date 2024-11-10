7b3e5d4f17b7d30c3ffb152b71f335f89ff20858
fnabinash <fnabinash@gmail.com>
2024-11-10 11:21:41 +0530

    [Error] commands/push.rs

--- src/commands/push.rs
+++ src/commands/push.rs
@@ -3,14 +3,14 @@ use std::process::Command;
 
 use adof::get_adof_dir;
 
-pub fn push() {
-    let adof_dir = get_adof_dir();
-    env::set_current_dir(adof_dir).unwrap();
+pub fn push() -> Result<()> {
+    let adof_dir = get_adof_dir()?;
+    env::set_current_dir(adof_dir)?;
 
     Command::new("git")
         .arg("push")
         .arg("origin")
         .arg("main")
-        .output()
-        .unwrap();
+        .output()?;
+    Ok(())
 }
