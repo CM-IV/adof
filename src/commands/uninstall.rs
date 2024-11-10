dfc3624c46bab191a1f8a163eb16f1accccba352
fnabinash <fnabinash@gmail.com>
2024-11-10 11:44:27 +0530

    [Error] commands/uninstall.rs

--- src/commands/uninstall.rs
+++ src/commands/uninstall.rs
@@ -4,13 +4,13 @@ use std::process::Command;
 
 use adof::{get_adof_dir, get_home_dir};
 
-pub fn uninstall() {
-    let adof_dir = get_adof_dir();
+pub fn uninstall() -> Result<()> {
+    let adof_dir = get_adof_dir()?;
     let pid_file = format!("{}/do_not_touch/pid.txt", adof_dir);
 
-    if fs::exists(&pid_file).unwrap() {
-        let pid = fs::read_to_string(&pid_file).unwrap();
-        Command::new("kill").arg(pid.trim()).output().unwrap();
+    if fs::exists(&pid_file)? {
+        let pid = fs::read_to_string(&pid_file)?;
+        Command::new("kill").arg(pid.trim()).output()?;
     }
 
     let home_dir = get_home_dir();
@@ -20,19 +20,23 @@ pub fn uninstall() {
     let adof_dir = get_adof_dir();
     remove_dir(&adof_dir);
 
-    let output = Command::new("which").arg("adof").output().unwrap();
-    let adof_bin_dir = String::from_utf8(output.stdout).unwrap();
+    let output = Command::new("which").arg("adof").output()?;
+    let adof_bin_dir = String::from_utf8(output.stdout)?;
     remove_dir(adof_bin_dir.trim());
+
+    Ok(())
 }
 
-fn remove_dir(dir: &str) {
+fn remove_dir(dir: &str) -> Result<()> {
     let path = Path::new(dir);
 
     if path.is_dir() {
-        fs::remove_dir_all(path).unwrap();
+        fs::remove_dir_all(path)?;
     }
 
     if path.is_file() {
-        fs::remove_file(path).unwrap();
+        fs::remove_file(path)?;
     }
+
+    Ok(())
 }
