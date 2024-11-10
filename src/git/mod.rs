f869af26afcc9c4d2c1762b37dfd726f84bb1311
fnabinash <fnabinash@gmail.com>
2024-11-10 12:11:34 +0530

    [Error] git/mod.rs

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use chrono::Local;
 use git2::Repository;
 
@@ -9,28 +10,31 @@ pub mod commit_message;
 pub mod git_ignore;
 pub mod remote;
 
-pub fn init_git() {
+pub fn init_git() -> Result<()> {
     let adof_dir = get_adof_dir();
-    Repository::init(adof_dir).unwrap();
-    add::git_add()
+    Repository::init(adof_dir)?;
+    add::git_add()?;
+    Ok(())
 }
 
-pub fn get_repo() -> Repository {
-    let adof_dir = get_adof_dir();
-    Repository::open(adof_dir).unwrap()
+pub fn get_repo() -> Result<Repository> {
+    let adof_dir = get_adof_dir()?;
+    let repo = Repository::open(adof_dir)?;
+    Ok(repo)
 }
 
 pub fn is_remote_exist() -> bool {
     get_repo().find_remote("origin").is_ok()
 }
 
-pub fn get_default_branch() -> String {
+pub fn get_default_branch() -> Result<String> {
     let repo = get_repo();
-    let config = repo.config().unwrap();
+    let config = repo.config()?;
+    let mut default_branch = "master".to_string();
 
-    if let Ok(default_branch) = config.get_string("init.defaultBranch") {
-        default_branch.to_string()
-    } else {
-        "master".to_string()
+    if let Ok(branch) = config.get_string("init.defaultBranch") {
+        default_branch = branch.to_string();
     }
+
+    Ok(default_branch)
 }
