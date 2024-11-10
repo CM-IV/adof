d9cdbf74e7e0f1ca501da40de351c4cd1bc02fb7
fnabinash <fnabinash@gmail.com>
2024-11-10 11:45:30 +0530

    [Error] commands/unlink.rs

--- src/commands/unlink.rs
+++ src/commands/unlink.rs
@@ -1,9 +1,11 @@
 use crate::git::{is_remote_exist, remote::unlink_remote};
 
-pub fn unlink() {
+pub fn unlink() -> Result<()> {
     if is_remote_exist() {
-        unlink_remote();
+        unlink_remote()?;
     } else {
         println!("first connect to remote");
     }
+
+    Ok(())
 }
