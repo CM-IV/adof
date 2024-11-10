b60744a17577765ea1adf2d7f2930ca9f8d1d841
fnabinash <fnabinash@gmail.com>
2024-11-10 10:49:11 +0530

    [Error] commands/auto_update.rs

--- src/commands/auto_update.rs
+++ src/commands/auto_update.rs
@@ -11,28 +11,27 @@ use crate::database::get_table_struct;
 use crate::git::add::git_add;
 
 #[allow(unreachable_code)]
-pub async fn auto_update(min: u64) {
-    if Path::new(&get_pid_file()).exists() {
-        process::exit(1) // change to return Ok(()) - because exit prints something to screen when
-                         // open a new terminal window.
+pub async fn auto_update(min: u64) -> Result<()> {
+    if Path::new(&get_pid_file()?).exists() {
+        return Ok(())
     }
 
-    store_pid();
+    store_pid()?;
 
     loop {
-        update();
+        update()?;
         sleep(Duration::from_secs(min * 60)).await;
     }
 
-    delete_pid_file();
+    delete_pid_file()?;
 }
 
-fn update() {
+fn update() -> Result<()> {
     let mut files_to_update: Vec<(String, String)> = Vec::new();
 
     let table_struct = get_table_struct();
     table_struct.table.iter().for_each(|data| {
-        if is_to_modify(data.0, data.1) {
+        if is_to_modify(data.0, data.1)? {
             files_to_update.push((data.0.to_string(), data.1.to_string()));
         }
     });
@@ -41,25 +40,27 @@ fn update() {
         files_to_update
             .iter()
             .for_each(|(original_file, backedup_file)| {
-                fs::copy(original_file, backedup_file).unwrap();
+                fs::copy(original_file, backedup_file)?;
             });
 
         git_add();
     }
 }
 
-fn is_to_modify(original_file: &str, backedup_file: &str) -> bool {
-    let original_hash = calculate_file_hash(original_file);
-    let backup_hash = calculate_file_hash(backedup_file);
-    original_hash != backup_hash
+fn is_to_modify(original_file: &str, backedup_file: &str) -> Result<bool> {
+    let original_hash = calculate_file_hash(original_file)?;
+    let backup_hash = calculate_file_hash(backedup_file)?;
+    Ok(original_hash != backup_hash)
 }
 
-fn store_pid() {
+fn store_pid() -> Result<()> {
     let pid_file = get_pid_file();
     let pid = process::id();
-    fs::write(&pid_file, pid.to_string()).unwrap();
+    fs::write(&pid_file, pid.to_string())?;
+    Ok(())
 }
 
-fn delete_pid_file() {
-    fs::remove_file(get_pid_file()).unwrap();
+fn delete_pid_file() -> Result<()> {
+    fs::remove_file(get_pid_file())?;
+    Ok(())
 }
