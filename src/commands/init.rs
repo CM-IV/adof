ae8f7ecef88e26ded9f89214e3557c3442e496cc
fnabinash <fnabinash@gmail.com>
2024-11-10 10:13:57 +0530

    [Refactor] Add error handling to commands/init.rs

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -15,7 +15,7 @@ use crate::{
 
 use super::*;
 
-pub async fn init() {
+pub async fn init() -> Result<()> {
     if check_for_init() {
         println!("Already initialized");
         process::exit(1);
@@ -27,38 +27,39 @@ pub async fn init() {
     let readme_task = tokio::spawn(async { create_readme().await });
     let found_files_task = tokio::spawn(async { find_files() });
 
-    readme_task.await.unwrap();
-    let found_files = found_files_task.await.unwrap();
+    readme_task.await?;
+    let found_files = found_files_task.await?;
 
     let selected_files = select_files(found_files);
 
     create_backup_files(&selected_files);
     init_git();
+
+    Ok(())
 }
 
-fn find_files() -> Vec<PathBuf> {
-    let home_dir = get_home_dir();
+fn find_files() -> Result<Vec<PathBuf>> {
+    let home_dir = get_home_dir()?;
 
     let mut found_files = Vec::new();
 
     for pattern in FILE_PATTERNS {
         let pattern_path = format!("{}/{}", home_dir, pattern);
 
-        for entry in glob(&pattern_path).expect("Failed to read glob pattern") {
-            match entry {
-                Ok(path) => found_files.push(path),
-                Err(e) => eprintln!("Error: {:?}", e),
-            }
+        for entry in glob(&pattern_path)? {
+            found_files.push(entry?);
         }
     }
 
-    found_files
+    Ok(found_files)
 }
 
-fn create_backup_files(selected_files: &[String]) {
+fn create_backup_files(selected_files: &[String]) -> Result<()> {
     (0..selected_files.len()).for_each(|i| {
-        let backup_file = create_file(&selected_files[i]);
-        fs::copy(&selected_files[i], &backup_file).unwrap();
+        let backup_file = create_file(&selected_files[i])?;
+        fs::copy(&selected_files[i], &backup_file)?;
         add::add_files(&selected_files[i], &backup_file);
     });
+
+    Ok(())
 }
