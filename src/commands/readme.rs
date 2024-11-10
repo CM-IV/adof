68ea03348416f6c6bbcff2666f06b2ae3a330aa5
fnabinash <fnabinash@gmail.com>
2024-11-10 11:36:23 +0530

    [Error] commands/readme.rs

--- src/commands/readme.rs
+++ src/commands/readme.rs
@@ -4,35 +4,37 @@ use adof::{get_adof_dir, get_home_dir};
 
 use crate::database::add;
 
-pub async fn create_readme() {
-    let local_readme = create_local_readme().await;
-    create_backup_readme(&local_readme);
+pub async fn create_readme() -> Result<()> {
+    let local_readme = create_local_readme().await?;
+    create_backup_readme(&local_readme)?;
+    Ok(())
 }
 
-async fn create_local_readme() -> String {
+async fn create_local_readme() -> Result<String> {
     let home_dir = get_home_dir();
     let local_readme_dir = format!("{}/dotfiles_readme", home_dir);
 
-    fs::create_dir_all(&local_readme_dir).unwrap();
+    fs::create_dir_all(&local_readme_dir)?;
 
     let local_readme_file_path = format!("{}/README.md", local_readme_dir);
-    fs::File::create(&local_readme_file_path).unwrap();
+    fs::File::create(&local_readme_file_path)?;
 
     let url =
         "https://raw.githubusercontent.com/fnabinash/adof/refs/heads/main/src/commands/README.md";
-    let response = reqwest::get(url).await.unwrap().text().await.unwrap();
+    let response = reqwest::get(url).await?.text().await?;
 
-    fs::write(&local_readme_file_path, response.as_bytes()).unwrap();
+    fs::write(&local_readme_file_path, response.as_bytes())?;
 
-    local_readme_file_path
+    Ok(local_readme_file_path)
 }
 
-fn create_backup_readme(local_readme_file: &str) {
-    let adof_dir = get_adof_dir();
+fn create_backup_readme(local_readme_file: &str) -> Result<()> {
+    let adof_dir = get_adof_dir()?;
     let backup_readme_file = format!("{}/README.md", adof_dir);
 
-    fs::File::create(&backup_readme_file).unwrap();
-    fs::copy(local_readme_file, &backup_readme_file).unwrap();
+    fs::File::create(&backup_readme_file)?;
+    fs::copy(local_readme_file, &backup_readme_file)?;
 
-    add::add_files(local_readme_file, &backup_readme_file);
+    add::add_files(local_readme_file, &backup_readme_file)?;
+    Ok(())
 }
