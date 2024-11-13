8a9c1672b34f2807df5270ee847e0b267fe2d748
fnabinash <fnabinash@gmail.com>
2024-11-13 17:05:43 +0530

    [Fix] add unit test for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,26 @@ pub fn get_adof_dir() -> String {
     let home_dir = get_home_dir();
     format!("{}/{}", home_dir, ".adof")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    #[test]
+    fn test_get_home_dir() {
+        env::set_var("HOME", "/test/home");
+
+        let home_dir = get_home_dir();
+        assert_eq!(home_dir, "/test/home");
+    }
+
+    #[test]
+    fn test_get_adof_dir() {
+        env::set_var("HOME", "/test/home");
+
+        let adof_dir = get_adof_dir();
+        assert_eq!(adof_dir, "/test/home/.adof");
+    }
+}
+
